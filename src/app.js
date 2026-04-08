9b31c7f79387ac0e2655377b2954659d452cda00
Arun Meena <Arunmeena.mca@gmail.com>
2026-04-08 23:53:10 +0530

    Second commit

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: "CI/CD Demo Running 🚀" });
+  res.json({ message: "CI/CD Demo Walking 🚀" });
 });
 
 module.exports = app;
