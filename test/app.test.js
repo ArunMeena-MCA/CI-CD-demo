ae6fa448c15768e962bf0ffb25792630da33412f
Arun Meena <Arunmeena.mca@gmail.com>
2026-04-09 00:39:20 +0530

    Corrected Test.js

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should return message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.body.message).toBe("CI/CD Demo Running 🚀");
+    expect(res.body.message).toBe("CI/CD Demo Walking 🚀");
   });
 });
